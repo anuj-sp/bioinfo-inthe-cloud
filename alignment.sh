1b968f5083509f7a682c015376eff870f083aa1d
anujsp2797 <47876183+anujsp2797@users.noreply.github.com>
2019-11-15 12:01:57 +0800

    Update alignment.sh

--- alignment.sh
+++ alignment.sh
@@ -4,31 +4,6 @@
 
 export LC_ALL=C
 
-Usage() {
-    cat <<EOF
-
-Usage: ./alignment.sh [options]
-
-Compulsory Arguments: 
-Query file type (i.e. 1 for sequence files in FASTA format or 2 for list of RefSeq IDs in .txt file)
-Query sequence in FASTA format (.fa file)
-Database file type (0 if using predetermined database or 1 for custom database using FASTA format or 2 for custom database with RefSeq IDs)
-Database file (0 if using predetermined database or .fa FASTA file or .txt RefSeq ID file)
-Google cloud virtual machine instance name 
-Sequence alignment program to use (0 for BLAST and 1 for Diamond)
-External ip address to allow for ssh into Google cloud instance
-ssh key to allow for ssh into Google cloud instance
-Username to allow for ssh into Google cloud instance 
-Output file name. The output file will be placed in the current working directory. 
-
-Ensure they are placed in the same order as mentioned. 
-
-EOF
-    exit 1
-}
-
-#[ "$1" = "" ] && Usage 
-
 TYPE="protein" 
 QUERYFILETYPE=$1
 QUERY=$2
